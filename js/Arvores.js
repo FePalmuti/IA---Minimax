cf276698fb14d1fa01f01e59cc56df532901e569
Felipe Palmuti <fepalmuti@gmail.com>
2019-09-27 12:10:01 -0300

    Refatoracao de metodos (IA -> Arvores)

--- js/Arvores.js
+++ js/Arvores.js
@@ -19,25 +19,6 @@ class Noh {
     }
 }
 
-class Arvore {
-    constructor(raiz) {
-        this.raiz = raiz;
-    }
-
-    imprimirRecursivo(noh) {
-        noh.imprimir();
-        if(noh.filhos.length != 0) {
-            for(let indice=0; indice<noh.filhos.length; indice++) {
-                this.imprimirRecursivo(noh.filhos[indice]);
-            }
-        }
-    }
-
-    imprimir() {
-        this.imprimirRecursivo(this.raiz);
-    }
-}
-
 class NohJogo extends Noh {
     constructor(id, jogoDaVelha) {
         super(id);
@@ -56,4 +37,110 @@ class NohMinimax extends Noh {
     }
 }
 
+class Arvore {
+    raiz = null
+    qntNohs = 0;
+
+    constructor() {}
+
+    autoGeracaoRecursiva() {}
+
+    imprimir() {
+        this.imprimirRecursivo(this.raiz);
+    }
+
+    imprimirRecursivo() {}
+}
+
+class ArvoreJogos extends Arvore {
+    constructor(jogoPrincipal) {
+        super();
+        this.raiz = new NohJogo(this.qntNohs, jogoPrincipal);
+        this.qntNohs++;
+        this.autoGeracaoRecursiva(this.raiz);
+    }
+
+    autoGeracaoRecursiva(noh) {
+        var jogadasDisponiveis = noh.jv.jogadasDisponiveis.slice();
+        var jogada, novoJV, nohFilho;
+        var filhos = [];
+        // Eh criado um novo filho para cada jogada possivel no jogo do noh pai
+        while(jogadasDisponiveis.length > 0) {
+            jogada = jogadasDisponiveis.pop();
+
+            novoJV = noh.jv.clonar();
+            var status = novoJV.fazerJogada(jogada);
+
+            nohFilho = new NohJogo(this.qntNohs, novoJV);
+            this.qntNohs++;
+            filhos.push(nohFilho);
+
+            if(status != "FIM") {
+                // Se o jogo nao terminou, as jogadas possiveis sao feitas
+                // em nohs filhos
+                this.autoGeracaoRecursiva(nohFilho);
+            }
+        }
+        noh.adicionarFilhos(filhos);
+    }
+
+    imprimirRecursivo(noh) {
+        noh.jv.imprimirJogo();
+        for(let indice=0; indice<noh.filhos.length; indice++) {
+            this.imprimirRecursivo(noh.filhos[indice]);
+        }
+    }
+}
+
+class ArvoreMinimax extends Arvore {
+    constructor(raizJogo, simboloIA) {
+        super();
+        this.simboloIA = simboloIA;
+        this.raiz = new NohMinimax(raizJogo.id, true, null);
+        this.autoGeracaoRecursiva(raizJogo, this.raiz);
+    }
+
+    autoGeracaoRecursiva(nohJogo, nohMinimax) {
+        var filhos = [];
+        var nohJogoFilho, id, vencedor, valor, novoNohMinimax;
+        var nivelTipoMax = ! nohMinimax.nivelTipoMax;
+        for(let indice=0; indice<nohJogo.filhos.length; indice++) {
+            nohJogoFilho = nohJogo.filhos[indice];
+            id = nohJogoFilho.id;
+            if(nohJogoFilho.jv.fimDeJogo) {
+                vencedor = nohJogoFilho.jv.vencedor;
+                valor = this.calcularValorNohMinimax(vencedor);
+                novoNohMinimax = new NohMinimax(id, nivelTipoMax, valor);
+                filhos.push(novoNohMinimax);
+            }
+            else {
+                novoNohMinimax = new NohMinimax(id, nivelTipoMax, null);
+                filhos.push(novoNohMinimax);
+                this.autoGeracaoRecursiva(nohJogoFilho, novoNohMinimax);
+            }
+        }
+        nohMinimax.adicionarFilhos(filhos);
+    }
+
+    calcularValorNohMinimax(vencedor) {
+        // Se a IA vence nesse noh
+        if(vencedor == this.simboloIA) {
+            return 1;
+        }
+        else if(vencedor == "NINGUEM") {
+            return 0;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    imprimirRecursivo(noh) {
+        var representacao = noh.id+" "+noh.nivelTipoMax+" "+noh.valor;
+        alert(representacao);
+        for(let indice=0; indice<noh.filhos.length; indice++) {
+            this.imprimirRecursivo(noh.filhos[indice]);
+        }
+    }
+}
 //
--- js/IA.js
+++ js/IA.js
@@ -2,110 +2,27 @@ class IA {
     arvoreJogos = null;
     arvoreMinimax = null;
     // Indica qual eh o simbolo representa a IA no jogo
-    simbolo = "";
+    simboloIA = "";
 
-    constructor(jogoPrincipal, simbolo) {
+    constructor(jogoPrincipal, simboloIA) {
         this.jogoPrincipal = jogoPrincipal;
-        this.simbolo = simbolo;
+        this.simboloIA = simboloIA;
         this.qntNohs = 0;
     }
 
     gerarArvoreJogos() {
-        var nohRaiz = new NohJogo(this.qntNohs, this.jogoPrincipal);
-        this.qntNohs++;
-        this.arvoreJogos = new Arvore(nohRaiz);
-        this.gerarArvoreJogosRecursivo(nohRaiz);
-    }
-
-    // Metodo recursivo para geracao de toda arvore
-    gerarArvoreJogosRecursivo(noh) {
-        var jogadasDisponiveis = noh.jv.jogadasDisponiveis.slice();
-        var jogada, novoJV, nohFilho;
-        var filhos = [];
-        // Eh criado um novo filho para cada jogada possivel no jogo do noh pai
-        while(jogadasDisponiveis.length > 0) {
-            jogada = jogadasDisponiveis.pop();
-
-            novoJV = noh.jv.clonar();
-            var status = novoJV.fazerJogada(jogada);
-
-            nohFilho = new NohJogo(this.qntNohs, novoJV);
-            this.qntNohs++;
-            filhos.push(nohFilho);
-
-            if(status != "FIM") {
-                // Se o jogo nao terminou, as jogadas possiveis sao feitas
-                // em nohs filhos
-                this.gerarArvoreJogosRecursivo(nohFilho);
-            }
-        }
-        noh.adicionarFilhos(filhos);
+        this.arvoreJogos = new ArvoreJogos(this.jogoPrincipal);
     }
 
     mostrarJogosEmProfundidade() {
-        var raiz = this.arvoreJogos.raiz;
-        this.mostrarJogosEmProfundidadeRecursivo(raiz);
-    }
-
-    mostrarJogosEmProfundidadeRecursivo(noh) {
-        noh.jv.imprimirJogo();
-        for(let indice=0; indice<noh.filhos.length; indice++) {
-            this.mostrarJogosEmProfundidadeRecursivo(noh.filhos[indice]);
-        }
-    }
-
-    mostrarNohsMinimaxEmProfundidade() {
-        var raiz = this.arvoreMinimax.raiz;
-        this.mostrarNohsMinimaxEmProfundidadeRecursivo(raiz);
-    }
-
-    mostrarNohsMinimaxEmProfundidadeRecursivo(noh) {
-        var representacao = noh.id+" "+noh.nivelTipoMax+" "+noh.valor;
-        alert(representacao);
-        for(let indice=0; indice<noh.filhos.length; indice++) {
-            this.mostrarNohsMinimaxEmProfundidadeRecursivo(noh.filhos[indice]);
-        }
-    }
-
-    calcularValorNohMinimax(vencedor) {
-        // Se a IA vence nesse noh
-        if(vencedor == this.simbolo) {
-            return 1;
-        }
-        else if(vencedor == "NINGUEM") {
-            return 0;
-        }
-        else {
-            return -1;
-        }
+        this.arvoreJogos.imprimir();
     }
 
     gerarArvoreMinimax() {
-        var raizJogo = this.arvoreJogos.raiz;
-        var raizMinimax = new NohMinimax(raizJogo.id, true, null);
-        this.arvoreMinimax = new Arvore(raizMinimax);
-        this.gerarArvoreMinimaxRecursivo(raizJogo, raizMinimax);
+        this.arvoreMinimax = new ArvoreMinimax(this.arvoreJogos.raiz, this.simboloIA);
     }
 
-    gerarArvoreMinimaxRecursivo(nohJogo, nohMinimax) {
-        var filhos = [];
-        var nohJogoFilho, id, vencedor, valor, novoNohMinimax;
-        var nivelTipoMax = ! nohMinimax.nivelTipoMax;
-        for(let indice=0; indice<nohJogo.filhos.length; indice++) {
-            nohJogoFilho = nohJogo.filhos[indice];
-            id = nohJogoFilho.id;
-            if(nohJogoFilho.jv.fimDeJogo) {
-                vencedor = nohJogoFilho.jv.vencedor;
-                valor = this.calcularValorNohMinimax(vencedor);
-                novoNohMinimax = new NohMinimax(id, nivelTipoMax, valor);
-                filhos.push(novoNohMinimax);
-            }
-            else {
-                novoNohMinimax = new NohMinimax(id, nivelTipoMax, null);
-                filhos.push(novoNohMinimax);
-                this.gerarArvoreMinimaxRecursivo(nohJogoFilho, novoNohMinimax);
-            }
-        }
-        nohMinimax.adicionarFilhos(filhos);
+    mostrarNohsMinimaxEmProfundidade() {
+        this.arvoreMinimax.imprimir();
     }
 }
